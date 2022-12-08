e4f0a162957d2bf98cff63fb77ddb56322847f29
Kipkirwa <gleitich@gmail.com>
2022-12-08 03:34:57 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,11 @@
-#include <stdio.h>
-
+#include <stdio>
 /**
+ * main - Entry point
  *
- *  * main - main block
- *
- *   * Return: 0
- *
- *    */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
-
 }
