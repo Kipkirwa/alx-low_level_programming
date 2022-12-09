e31309f8b069cad5afc3a3c7c04cc7da4727cb88
Kipkirwa <gleitich@gmail.com>
2022-12-09 04:56:14 -0800

    task 5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+{
+	int main(void)
+		char letter;
+		for(letter = 'a'; letter <= 'z'; letter++)
+			{
+				if(letter != 'e' && letter != 'q')
+					putchar(letter);
+			}
+		putchar('\n');
+		return (0);
+}
