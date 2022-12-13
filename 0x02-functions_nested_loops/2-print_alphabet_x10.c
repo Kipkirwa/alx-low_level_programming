cf0c8645874ba11b05d3cea1692a1e9367cf51c4
Kipkirwa <gleitich@gmail.com>
2022-12-13 04:22:08 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print_alphabet_x10.c
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void);
+
+{
+	int count = 0;
+	char letter;
+
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
