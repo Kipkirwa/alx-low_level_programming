dba99fc73a10a9c49c8fd0cd3bacf8fa02f7b7e6
Kipkirwa <gleitich@gmail.com>
2022-12-20 03:15:15 -0800

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,3 +17,19 @@ void puts_half(char *str)
 
 	putchar(10);
 }
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: the length of the given string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
